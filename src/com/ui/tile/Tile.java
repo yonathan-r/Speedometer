90bccb0aa878979e6144f1304a7867cab7474f2a
Yonathan Redda <yredda@ford.com>
2015-05-13 19:26:45 -0700

    delete build files, rename application package

--- src/com/ui/tile/Tile.java
+++ src/com/ui/tile/Tile.java
@@ -1,6 +1,6 @@
-package com.demo.controls;
+package com.ui.tile;
 
-import com.demo.speedometer.R;
+import com.ui.tile.R;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
